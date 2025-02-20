b8c571fc5838c320fc8420cafb4cdcf8de73d605
HIMANSHU TIWARI <119152532+himanshutiwari2709@users.noreply.github.com>
2025-02-20 05:32:34 +0530

    Update MongoDB connection to use Atlas URI

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -12,7 +12,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/db123';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI)
 .then(() => console.log("MongoDB Connected Successfully to:", MONGODB_URI))
